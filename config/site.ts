8ba72744d62d5aafd2573842c39ed5ab7154e612
LeandroTanner <leandrotanner2@gmail.com>
2026-03-02 22:41:56 -0300

    Atualizando configuração da url para o telegram.

--- config/site.ts
+++ config/site.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
     linkedin: 'https://www.linkedin.com/in/leandrotanner/',
     email: 'ltannerdev@gmail.com',
     instagram: 'https://www.instagram.com/leandro_a.tanner/',
-    telegram: 'https://t.me/+5519989542317?text=Ol%C3%A1,%20Leandro!%20Adorei%20seu%20site%20e%20gostaria%20de%20conversar%20com%20voc%C3%AA.%20Seu%20portf%C3%B3lio%20chamou%20minha%20aten%C3%A7%C3%A3o%20e%20estou%20interessado%20em%20discutir%20oportunidades.%20Por%20favor,%20me%20retorne%20quando%20poss%C3%ADvel',
+    telegram: 'https://t.me/ltanner1?text=Ol%C3%A1,%20Leandro!%20Adorei%20seu%20site%20e%20gostaria%20de%20conversar%20com%20voc%C3%AA.%20Seu%20portf%C3%B3lio%20chamou%20minha%20aten%C3%A7%C3%A3o%20e%20estou%20interessado%20em%20discutir%20oportunidades.%20Por%20favor,%20me%20retorne%20quando%20poss%C3%ADvel',
     cvUrl: 'https://drive.google.com/file/d/1t5ol89AlwipFuTd4j_ompj50hZy_nfql/view',
   },
 };
